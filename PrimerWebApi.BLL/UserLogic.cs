3df5d0bd46a03e176be90d6acf4a9262835e3f14
pollosatyi <pollosatyi@mail.ru>
2024-03-19 17:40:03 +0300

    Добавьте файлы проекта.

---
+++ PrimerWebApi.BLL/UserLogic.cs
@@ -0,0 +1,18 @@
+﻿using PrimerWebApi.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimerWebApi.BLL
+{
+    public class UserLogic : IUserLogic
+    {
+        private IUserRepository _userRepository;
+        public UserLogic(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+    }
+}
---
+++ PrimerWebApi.Common/User.cs
@@ -0,0 +1,10 @@
+﻿namespace PrimerWebApi.Common.Users
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+
+        public string lastname { get; set; }
+    }
+}
