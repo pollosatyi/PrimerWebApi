a77c63f048f56b6bee5c693ee671cfc834d18c82
pollosatyi <pollosatyi@mail.ru>
2024-03-19 21:02:15 +0300

    сохранение перед запуском

--- PrimerWebApi/Program.cs
+++ PrimerWebApi/Program.cs
@@ -1,3 +1,6 @@
+using PrimerWebApi.BLL;
+using PrimerWebApi.DAL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IUserLogic, UserLogic>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 var app = builder.Build();
 
